4b5c317bd7f2a15162f5006f4b37d5b5800ac372
MayumiYesenia <104656102+MayumiYesenia@users.noreply.github.com>
2024-05-19 20:09:28 -0500

    Update api-interaction.py

--- api-interaction.py
+++ api-interaction.py
@@ -79,4 +79,4 @@ def delete_favorite_image(id):
         return jsonify({"error": "Imagen favorita no encontrada"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8002, debug=True)
+    app.run(host='0.0.0.0', port=8003, debug=True)
